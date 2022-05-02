04d4b4eccf026ff1c8b6023c92849f390fc373b4
francovega <francovega1234@gmail.com>
2022-05-02 11:48:08 -0300

    Generardor de codigos y clasificacion de envios

--- Source code homework/OCASA.py
+++ Source code homework/OCASA.py
@@ -9,5 +9,28 @@
     #lucksJa
 
 #Descomentar para probar
-#import data as d
+import random 
+import data as d
 #print(d.obtenerDatos())
+
+#generador de codigo de seguimiento 
+longitud = 8
+valores = "0123456789"
+aux = ""
+for codigo in d.obtenerDatos():
+    longitud = 8
+    valores = "0123456789"
+    aux = ""
+    aux = aux.join([random.choice(valores) for i in range(longitud)])
+    codigo ['cod']= "OC" + aux
+  
+#Clasificacion de entregas
+destino_entregado=[]
+destino_no_entregado=[]
+for fichas_de_envio in d.obtenerDatos()  :
+    if fichas_de_envio ['entregado'] == 1 : 
+        destino_entregado.append( fichas_de_envio ['dst'])
+    else:
+        destino_no_entregado.append( fichas_de_envio ['dst'])
+# print (destino_entregado)
+# print (destino_no_entregado)

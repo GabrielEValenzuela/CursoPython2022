cef85a7ae409265d2bf2337239d6dacd744fe1bf
Tiziano <tiziquevedo22@gmail.com>
2022-05-14 02:23:56 -0300

    maximo de entregas

--- Source code homework/OCASA.py
+++ Source code homework/OCASA.py
@@ -81,4 +81,6 @@ maximos_envios=nombre_ciudades[envios_por_ciudades.index(max(envios_por_ciudades
 #en el bloque anterior, lo que hago es sacar el valor maximo de la lista envios_por_ciudades, luego obtengo la posicion de 
 #dicho valor para luego ir a la misma posicion en nombre_ciudades y atribuirle el nombre a la variable maximos_envios
 print ('La mayor cantidad de envios se realizan en: ', maximos_envios)
-
+#Intente hacer la maxima de entregas pero nose cual es mi error
+maximo_de_entregas=destino_entregado[entrega.index(max(entrega))]
+print ("La mayor cantidad de entregas se realiza en: ", maximo_de_entregas)
